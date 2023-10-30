99fe6848905554db2e4c1f83b622c5dbba0eef26
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2023-10-30 18:08:52 +0100

    finish project

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const addNoteBtn = document.querySelector(".add-note");
 const overlay = document.querySelector(".overlay");
 const noteModal = document.querySelector(".note-modal");
 const noteConfirm = document.querySelector(".add");
+const closeModal = document.querySelector(".close");
+const inputTitle = document.querySelector(".note-up-input");
+const inputDescription = document.querySelector(".note-down-desc");
+const noteContainer = document.querySelector(".create-note");
 
 const addNewNote = function () {
   if (
@@ -16,5 +20,49 @@ const addNewNote = function () {
   }
 };
 
+const updateAddButtonAvailability = function () {
+  const isTitleEmpty = inputTitle.value.trim() === "";
+  const isDescriptionEmpty = inputDescription.value.trim() === "";
+  noteConfirm.disabled = isTitleEmpty || isDescriptionEmpty;
+};
+
+const createNote = function () {
+  const r = Math.floor(Math.random() * 70);
+  const g = Math.floor(Math.random() * 150);
+  const b = Math.floor(Math.random() * 150);
+
+  const title = inputTitle.value;
+  const description = inputDescription.value;
+
+  addNewNote();
+
+  const html = ` <div class="note" style="background-color: rgb(${r}, ${g}, ${b})">
+  <i class="fas fa-times"></i>
+  <h3 class="note-title">${title}</h3>
+  <p class="note-description">
+    ${description}
+  </p>
+</div>`;
+
+  if (!noteConfirm.disabled) {
+    noteContainer.insertAdjacentHTML("beforeend", html);
+    inputTitle.value = "";
+    inputDescription.value = "";
+  }
+
+  const deleteNoteBtn = document.querySelector(".fa-times");
+  deleteNoteBtn.addEventListener("click", deleteNote);
+};
+
+const deleteNote = function (e) {
+  e.target.closest(".note").remove();
+};
+
+inputTitle.addEventListener("input", updateAddButtonAvailability);
+inputDescription.addEventListener("input", updateAddButtonAvailability);
+
 addNoteBtn.addEventListener("click", addNewNote);
-noteConfirm.addEventListener("click", addNewNote);
+noteConfirm.addEventListener("click", createNote);
+closeModal.addEventListener("click", addNewNote);
+
+updateAddButtonAvailability();
